1b8bcb648fb1e64241c4afb32111e44d1ac78657
Igor Tudisco <igortudisco@hotmail.com>
2024-12-09 16:04:59 -0300

    Deleting Classes

--- ScreenSound/Data/DAL.cs
+++ ScreenSound/Data/DAL.cs
@@ -45,3 +45,16 @@ internal abstract class DAL<T> where T : class // Indicando que a ref de T será
     }
 
 }
+
+
+/*
+ 
+ * No C#, trabalhamos com o Func para encapsular um método que geralmente tem um parâmetro e retorna um valor do tipo especificado pelo parâmetro TResult.
+ 
+ * Ele é muito útil para situações como a que passamos no vídeo anterior, quando precisamos representar um método sem ter que passar especificamente valores
+ * personalizados, deixando a correspondência para a assinatura do método.
+ 
+ * Existem outras variações de utilização do Func, com mais parâmetros e condições, por exemplo, e para conhecer mais a fundo sobre eles e ver exemplos de
+ * aplicação você pode consultar a documentação oficial (https://learn.microsoft.com/pt-br/dotnet/api/system.func-2?view=net-8.0).
+ 
+ */
