4ae9936310972efa5384c81147a8c14eb234af03
Igor Tudisco <igortudisco@hotmail.com>
2024-12-10 13:51:43 -0300

    Adding the first Migration

--- ScreenSound/Data/ScreenSoundContext.cs
+++ ScreenSound/Data/ScreenSoundContext.cs
@@ -51,3 +51,21 @@ internal class ScreenSoundContext : DbContext
  * com mais informações sobre as convenções do Entity e exemplos de aplicação.
  
  */
+
+
+/*
+ 
+ * No contexto de banco de dados, utilizamos as migrations para gerenciar alterações no banco de dados. E no contexto em que estamos trabalhando
+ * com o Entity Framework, as migrations são uma maneira de controlar as alterações no banco de dados com base nas modificações do projeto de maneira
+ * controlada e automatizada.
+ 
+ * Através delas conseguimos fazer inclusão e exclusão de tabelas, alterações de colunas e mudanças de informações, tudo isso atrelado à evolução
+ * e crescimento do projeto de forma estruturada.
+ 
+ * Além disso, utilizando as migrations, é possível ter um histórico estruturado das alterações que ocorreram no banco de dados, facilitando o trabalho
+ * em equipe e também as atualizações em diversos ambientes existentes.
+ 
+ * Para saber mais sobre as migrations, você pode consultar a documentação oficial.
+ * (https://learn.microsoft.com/pt-br/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli)
+ 
+ */
