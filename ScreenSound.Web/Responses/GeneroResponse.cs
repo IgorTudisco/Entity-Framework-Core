38220fa6a89d296c7b14548c09038b6ee86ca833
Igor Tudisco <igortudisco@hotmail.com>
2025-01-09 15:34:31 -0300

    Creating services Music, Genero, and also a page of CadastrarMusica

--- ScreenSound.Web/Responses/GeneroResponse.cs
+++ ScreenSound.Web/Responses/GeneroResponse.cs
@@ -1,3 +1,9 @@
 ﻿namespace ScreenSound.Web.Response;
 
-public record class GeneroResponse(int IdGenero, string? Nome, string? Descricao);
+public record class GeneroResponse(int IdGenero, string? Nome, string? Descricao)
+{
+    public override string ToString()
+    {
+        return $"{this.Nome}";
+    }
+};
