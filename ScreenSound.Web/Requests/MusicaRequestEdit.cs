2704a494dd80b1056f62b8156d2f3c4ca615bb39
Igor Tudisco <igortudisco@hotmail.com>
2025-01-13 14:24:05 -0300

    Music editing has been implemented

--- ScreenSound.Web/Requests/MusicaRequestEdit.cs
+++ ScreenSound.Web/Requests/MusicaRequestEdit.cs
@@ -2,6 +2,6 @@
 
 namespace ScreenSound.Web.Requests;
 
-public record MusicaRequestEdit(int Id, string nome, int ArtistaId, int anoLancamento)
+public record MusicaRequestEdit(int Id, string nome, int ArtistaId, int? anoLancamento)
     : MusicaRequest(nome, ArtistaId, anoLancamento);
 
